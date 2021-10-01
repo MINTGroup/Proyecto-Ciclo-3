0b9c244d5e8f860ff5b48b0686ac3ddc22d685f0
xChubbs <alujan@unal.edu.co>
2021-10-01 16:20:04 -0500

    Se maqueto la tabla de productos, faltan datos

---
+++ Codigos/DataTable.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $.fn.dataTable.ext.classes.sPageButton = 'btn mx-1 btn-outline-dark rounded-3';
+    $('#data-table').DataTable( {
+        language: {
+            search: '<i class="fas fa-search"></i> ',
+            searchPlaceholder: 'Search'
+        },
+        columnDefs: [ {
+            orderable: false,
+            className: 'select-checkbox',
+            targets:   0
+        } ],
+        select: {
+            style:    'multi',
+            selector: 'td:first-child'
+        },
+        order: [[ 1, 'asc' ]],
+    } );
+} );
