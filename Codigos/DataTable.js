f360995a24c0cad7e846edcd2df3ddd691f55e42
xChubbs <alujan@unal.edu.co>
2021-10-01 19:09:28 -0500

    Se agregan blancos para rutas de agregar productos y ventas

--- Codigos/DataTable.js
+++ Codigos/DataTable.js
@@ -1,6 +1,24 @@
 $(document).ready(function() {
     $.fn.dataTable.ext.classes.sPageButton = 'btn mx-1 btn-outline-dark rounded-3';
-    $('#data-table').DataTable( {
+    $('#data-table-I').DataTable( {
+        language: {
+            search: '<i class="fas fa-search"></i> ',
+            searchPlaceholder: 'Search'
+        },
+        columnDefs: [ {
+            orderable: false,
+            className: 'select-checkbox',
+            targets:   0
+        } ],
+        select: {
+            style:    'multi',
+            selector: 'td:first-child'
+        },
+        order: [[ 1, 'asc' ]],
+    } );
+
+    $.fn.dataTable.ext.classes.sPageButton = 'btn mx-1 btn-outline-dark rounded-3';
+    $('#data-table-II').DataTable( {
         language: {
             search: '<i class="fas fa-search"></i> ',
             searchPlaceholder: 'Search'
