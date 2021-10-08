b7e90c0623acf2768e493f00686845ab2eda5162
Yulian Múnera <yuliannn94@gmail.com>
2021-10-07 21:50:19 -0500

    Cacharreando React

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './media/logo_MINT.svg'
+import './App.css'
+
+function App () {
+    return (
+        <div>
+            <header clasName="App-header">
+                <img src="./media/logo.png" className="App-logo" alt="logo" />
+                <h4>Hola. Soy un header.</h4>
+            </header>
+            <main></main>
+            <Footer>
+                <h4>Hola. Soy un Footer</h4>
+            </Footer>
+        </div>
+    )
+}
